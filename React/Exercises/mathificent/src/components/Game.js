328b4df2e76573f6373511babca4389d9a88278b
Lawrence Buta <larry.buta@gmail.com>
2021-12-01 21:45:46 -0500

    Interim Save of Advanced JS Concepts-12-01-2021

--- React/Exercises/mathificent/src/components/Game.js
+++ React/Exercises/mathificent/src/components/Game.js
@@ -12,6 +12,9 @@ function Game({operation, maxNumber}) {
     const [operands, setOperands] = useState(randNums);
     const question = operands.num1 + ' ' + operation + ' ' + operands.num2;
     const [userAnswer, setUserAnswer] = useState('');
+    const [score, setScore] = useState(0);
+    const [answered, setAnswered] = useState(false);
+    const equationClass = answered ? 'row my-2 text-primary fade' : 'row my-2 text-secondary';
 
     function appendToAnswer(num) {
         setUserAnswer(String(Number(userAnswer + num)));
@@ -24,18 +27,52 @@ function Game({operation, maxNumber}) {
         const numLow = Math.min(num1, num2);
         
         if(operator === '-') { // Make sure higher num comes first
-          num1 = numHigh;
-          num2 = numLow;
+            num1 = numHigh;
+            num2 = numLow;
         }
         
         if(operator === '/') {
             if (num2 === 0) { // No division by zero
-              num2 = randInt(1, high);
+                num2 = randInt(1, high);
             }
             num1 = (num1 * num2);
-          }
+            }
         return {num1, num2};
-      }
+    }
+
+    function checkAnswer(userAnswer) {
+        if (isNaN(userAnswer)) return false; // User hasn't answered
+        
+        let correctAnswer;
+        switch(operation) {
+            case '+':
+            correctAnswer = operands.num1 + operands.num2;
+            break;
+            case '-':
+            correctAnswer = operands.num1 - operands.num2;
+            break;
+            case 'x':
+            correctAnswer = operands.num1 * operands.num2;
+            break;
+            default: // division
+            correctAnswer = operands.num1 / operands.num2;
+        }
+        return (parseInt(userAnswer) === correctAnswer);
+    }
+
+    if (!answered && checkAnswer(userAnswer)) {
+        setAnswered(true);
+        setScore(score + 1);
+        setTimeout(newQuestion, 1000);
+    }
+
+    function newQuestion() {
+        setUserAnswer('');
+        setAnswered(false);
+        randNums=getRandNumbers(operation, 0, maxNumber);
+        setOperands(randNums);
+    }
+
     const numbers = [1,2,3,4,5,6,7,8,9,0];
     const numberButtons = numbers.map((number) => 
         <NumberButton value={number} key={number} handleClick={appendToAnswer} />); 
@@ -43,12 +80,12 @@ function Game({operation, maxNumber}) {
         <main className="text-center" id="game-container">
             <div className="col px-3 text-left" style={{fontSize: "1.5em"}}>
                 <div className="col px-3 text-left">
-                    <Score score="0" />
+                    <Score score={score} />
                 </div>
                 <div className="col px-3 text-right">
                     <Timer timeLeft="60" />
                 </div>
-                <div className="row text-secondary my-2" id="equation">
+                <div className={equationClass} id="equation">
                     <Equation question={question} answer={userAnswer} />
                 </div>
                 <div className="row" id="buttons">
